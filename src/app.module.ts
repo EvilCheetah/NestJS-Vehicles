ab06ff152c9f800baf37ac13e1842685fb9a4a75
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-03-14 11:35:03 -0700

    Updated imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MakeModule } from './make/make.module';
+import { ModelModule } from './model/model.module';
 
 
 @Module({
     imports:     [
         TypeOrmModule.forRoot(),
-        MakeModule
+        MakeModule,
+        ModelModule
     ],
 })
 export class AppModule {}
