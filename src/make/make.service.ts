fa02563ec23f4d1d469fffab24c768d020454f08
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-03-14 09:46:34 -0700

    Injected Repository into the Service

--- src/make/make.service.ts
+++ src/make/make.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateMakeDTO } from './dto/create-make.dto';
 import { UpdateMakeDTO } from './dto/update-make.dto';
+import { Make } from './entities/make.entity';
+
 
 @Injectable()
-export class MakeService {
-  create(createMakeDTO: CreateMakeDTO) {
-    return 'This action adds a new make';
-  }
+export class MakeService 
+{
+    constructor(
+        @InjectRepository(Make)
+        private readonly makeRepository: Repository<Make>
+    ) {}
 
-  findAll() {
-    return `This action returns all make`;
-  }
+    create(createMakeDTO: CreateMakeDTO) 
+    {
+        return 'This action adds a new make';
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} make`;
-  }
+    findAll() 
+    {
+        return `This action returns all make`;
+    }
 
-  update(id: number, updateMakeDTO: UpdateMakeDTO) {
-    return `This action updates a #${id} make`;
-  }
+    findOne(id: number) 
+    {
+        return `This action returns a #${id} make`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} make`;
-  }
+    update(id: number, updateMakeDTO: UpdateMakeDTO) 
+    {
+        return `This action updates a #${id} make`;
+    }
+
+    remove(id: number) 
+    {
+        return `This action removes a #${id} make`;
+    }
 }
