12370cc8d4b9948dfbf80c18b321d7a27592e2ed
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-03-14 11:33:17 -0700

    Added type-hinting

--- src/make/make.controller.ts
+++ src/make/make.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MakeService } from './make.service';
+
+import { Make } from './entities/make.entity';
 import { CreateMakeDTO } from './dto/create-make.dto';
 import { UpdateMakeDTO } from './dto/update-make.dto';
 
@@ -10,31 +12,31 @@ export class MakeController
     constructor(private readonly makeService: MakeService) {}
 
     @Post()
-    create(@Body() createMakeDTO: CreateMakeDTO) 
+    create(@Body() createMakeDTO: CreateMakeDTO): Promise<Make>
     {
         return this.makeService.create(createMakeDTO);
     }
 
     @Get()
-    findAll() 
+    findAll(): Promise<Make[]>
     {
         return this.makeService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) 
+    findOne(@Param('id') id: string): Promise<Make>
     {
         return this.makeService.findOne(+id);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updateMakeDTO: UpdateMakeDTO) 
+    update(@Param('id') id: string, @Body() updateMakeDTO: UpdateMakeDTO): Promise<Make>
     {
         return this.makeService.update(+id, updateMakeDTO);
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) 
+    remove(@Param('id') id: string): Promise<Make>
     {
         return this.makeService.remove(+id);
     }
