cdd12c6eb1e8633782163b1c33df49592e56b33c
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-03-14 11:35:43 -0700

    Updated formatting

--- src/model/model.service.ts
+++ src/model/model.service.ts
@@ -3,24 +3,30 @@ import { CreateModelDto } from './dto/create-model.dto';
 import { UpdateModelDto } from './dto/update-model.dto';
 
 @Injectable()
-export class ModelService {
-  create(createModelDto: CreateModelDto) {
-    return 'This action adds a new model';
-  }
+export class ModelService 
+{
+    create(createModelDto: CreateModelDto) 
+    {
+        return 'This action adds a new model';
+    }
 
-  findAll() {
-    return `This action returns all model`;
-  }
+    findAll() 
+    {
+        return `This action returns all model`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} model`;
-  }
+    findOne(id: number) 
+    {
+        return `This action returns a #${id} model`;
+    }
 
-  update(id: number, updateModelDto: UpdateModelDto) {
-    return `This action updates a #${id} model`;
-  }
+    update(id: number, updateModelDto: UpdateModelDto) 
+    {
+        return `This action updates a #${id} model`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} model`;
-  }
+    remove(id: number) 
+    {
+        return `This action removes a #${id} model`;
+    }
 }
